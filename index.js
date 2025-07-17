75324945c18c3fcbb7822cf1944796e555b237b1
fshusaku0903 <shusaku0903g@gmail.com>
2025-07-17 22:57:52 +0900

    chore: update ngrok URL & endpoints

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('.'));
 app.use('/files', express.static(path.join(__dirname, 'public/files')));
 
 const upload = multer({ dest: 'public/files' });
